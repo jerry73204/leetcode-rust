5b3cec0a4d6a357ec8d3128f4da63031521ac5ad
jerry73204 <jerry73204@gmail.com>
2023-08-11 17:29:06 +0800

    Solve p371.

--- src/lib.rs
+++ src/lib.rs
@@ -65,5 +65,7 @@ pub mod p944;
 pub mod p974;
 pub mod p989;
 pub mod p997;
+pub mod p371;
+
 
 pub struct Solution;
---
+++ src/p371.rs
@@ -0,0 +1,36 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn get_sum(mut a: i32, mut b: i32) -> i32 {
+        loop {
+            let c = (a & b) << 1;
+            let x = a ^ b;
+
+            if c == 0 {
+                break x;
+            }
+
+            a = x;
+            b = c;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+    use rand::prelude::*;
+
+    #[test]
+    fn p371_test() {
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..10000 {
+            let a = rng.gen_range(-1000, 1000);
+            let b = rng.gen_range(-1000, 1000);
+            let sum = a + b;
+            let ans = Solution::get_sum(a, b);
+            assert_eq!(ans, a + b, "{a} + {b} = {sum}, but get {ans}");
+        }
+    }
+}
